7d8f4c5ba7e10ef1c5a4090c350da5c5d053de13
Pham Hoang Lan <lanhoangpham.ph@gmail.com>
2022-04-03 09:03:59 +0700

    my position

--- views/js/main.js
+++ views/js/main.js
@@ -8,7 +8,7 @@ function changeContent(type, element){
     element.style.background = '#ccc';
 
     switch ( type ){
-        case 'honePage':
+        case 'homePage':
             document.querySelector('.honePage').style.display = 'block';
             document.querySelector('.infor').style.display = 'none';
             document.querySelector('.myWallet').style.display = 'none';
@@ -73,49 +73,95 @@ function closeUpdateInfor(){
 
 
 
-//javascript for hide/display buyticket_option1
-
-function openOption1(){
-    document.querySelector('.buyTicket_option1').style.display = 'flex';
-}
-function closeBuyticket1(){
-    document.querySelector('.buyTicket_option1').style.display = 'none';
-}
-//javascript for hide/display buyticket_option2
-
-function openOption2(){
-    document.querySelector('.buyTicket_option2').style.display = 'flex';
-}
-function closeBuyticket2(){
-    document.querySelector('.buyTicket_option2').style.display = 'none';
-}
-
-//javascript for hide/display buyticket_option1
-
-function openOption3(){
-    document.querySelector('.buyTicket_option3').style.display = 'flex';
-}
-function closeBuyticket3(){
-    document.querySelector('.buyTicket_option3').style.display = 'none';
-}
-
-
-
 
 
 
 
 //map
 
-let map;
+//let map;
 
+let infoWindow, map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
+    // The location of Uluru
+    const uluru = { lat: 21.00706853613633, lng: 105.84312793670026 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 12,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker_hust = new google.maps.Marker({
+      position: uluru,
+      map: map,
+      label: 'Trạm 1',
+      title: 'Số lượng xe: 2'
+    
+    });
 
+    
+    const marker_ussh = new google.maps.Marker({
+        position: {lat: 20.99510606882998, lng: 105.80775573911664},
+        map: map,
+        label: 'Trạm 2'
+        
+      });
+
+    
+    const marker_ulis = new google.maps.Marker({
+        position: {lat: 21.039302467560347, lng: 105.78179263911746},
+        map: map,
+        label: "Trạm 3"
+    });
+
+    // marker_ulis.setIcon('./assset/img/img-buyTicket.webp')
+
+    
+    
+      //display my position
+      infoWindow = new google.maps.InfoWindow();
+
+      const locationButton = document.createElement("button");
+    
+      locationButton.textContent = "Vị trí của tôi";
+      locationButton.classList.add("custom-map-control-button");
+      map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+      locationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              };
+    
+              infoWindow.setPosition(pos);
+              infoWindow.setContent("Location found.");
+            //   infoWindow.Marker.setIcon('./assset/img/img-buyTicket.webp');
+              infoWindow.open(map);
+              map.setCenter(pos);
+            },
+            () => {
+              handleLocationError(true, infoWindow, map.getCenter());
+            }
+          );
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      });
+}  
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(
+        browserHasGeolocation
+          ? "Error: The Geolocation service failed."
+          : "Error: Your browser doesn't support geolocation."
+      );
+      infoWindow.open(map);
+
+      }
 // logout
 
 function logoutFunc() {
